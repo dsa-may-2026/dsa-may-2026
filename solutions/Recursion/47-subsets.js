6a7ff02bfaaa2fcd892636a863b16b75e7748fb0
logicmojodsa <logicmojomentor@gmail.com>
2026-07-05 12:06:47 +0530

    Add solution: Subsets

---
+++ solutions/Recursion/47-subsets.js
@@ -0,0 +1,38 @@
+//works but polluted bag
+function subsets(nums){
+  let trolly = []
+
+  function helper(i, bag) {
+    if(i == nums.length) {
+      trolly.push([...bag])
+      return
+    }
+    bag.push(nums[i])
+    helper(i+1, bag)
+    bag.pop()
+    helper(i+1, bag)
+  }
+
+  helper(0, [])
+  return trolly
+}
+
+
+//ideal way
+function subsets(nums){
+
+  function helper(i) {
+    if(i==nums.length) {
+        return [[]];
+    }
+    let trolly = helper(i+1);
+    let newTrolly = [];
+    for(let bag of trolly) {
+        newTrolly.push([...bag]);
+        newTrolly.push([...bag, nums[i]]);
+    }
+    return newTrolly;
+  }
+
+  return helper(0)
+}
