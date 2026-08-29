46e4d7adb0dd449619976dc1c803a6936195654b
logicmojodsa <logicmojomentor@gmail.com>
2026-08-29 18:28:25 +0530

    Add solution: Graph Facebook Implementation

--- solutions/Graphs/77-graph-facebook-implementation.js
+++ solutions/Graphs/77-graph-facebook-implementation.js
@@ -61,14 +61,72 @@ class Facebook {
 
     return true
   }
+
+  bfs(uname) {
+    let res = []
+
+    let queue = [uname]
+
+    let visited = new Set()
+
+    while(queue.length) {
+      let levelSize = queue.length
+      let levelRes = []
+      while(levelSize--) {
+        let item = queue.shift()
+        visited.add(item)
+        levelRes.push(item)
+        for(let nextItem of this.users.get(item)) {
+          if(!visited.has(nextItem)) {
+            queue.push(nextItem)
+          }
+        }
+      }
+      res.push(levelRes)
+    }
+    return res
+  }
+
+  dfs(uname) {
+    let res = []
+    let visited = new Set()
+    
+    function helper(uname) {
+      res.push(uname)
+      visited.add(uname)
+      for(let nextItem of this.users.get(uname)) {
+        if(!visited.has(nextItem)) {
+          helper.call(this, nextItem)
+        }
+      }
+    }
+
+    helper.call(this,uname)
+    return res
+  }
+  
 }
 
 let fb = new Facebook()
-fb.signUp("venky")
-fb.signUp("phan")
-fb.signUp("sus")
+fb.signUp("A")
+fb.signUp("N")
+fb.signUp("K")
+fb.signUp("M")
+fb.signUp("B")
+fb.signUp("O")
+fb.signUp("P")
+fb.signUp("L")
+fb.signUp("C")
+fb.signUp("D")
 
-fb.addFriend("venky", "mark")
-fb.addFriend("venky", "phan")
-fb.deleteUser("zuck")
-fb.deleteUser("venky")
+fb.addFriend("A", "B")
+fb.addFriend("A", "K")
+fb.addFriend("A", "M")
+fb.addFriend("A", "N")
+fb.addFriend("A", "O")
+fb.addFriend("O", "P")
+fb.addFriend("K", "L")
+fb.addFriend("B", "C")
+fb.addFriend("C", "D")
+
+fb.dfs("A")
