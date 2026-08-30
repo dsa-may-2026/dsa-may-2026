d5c591d95dd63bd382b9107a8b4071df9b65586e
logicmojodsa <logicmojomentor@gmail.com>
2026-08-30 18:21:51 +0530

    Add solution: Course Schedule II

---
+++ solutions/Graphs/85-course-schedule-ii.js
@@ -0,0 +1,51 @@
+/**
+ * @param {number} numCourses
+ * @param {number[][]} prerequisites
+ * @return {number[]}
+ */
+
+var findOrder = function (numCourses, prerequisites) {
+    let map = {}
+
+    for (let i = 0; i < numCourses; i++) {
+        map[i] = new Set()
+    }
+
+    for (let pre of prerequisites) {
+        let [dependent, dependency] = pre
+        map[dependent].add(dependency)
+    }
+
+    let queue = []
+
+    let res  = []
+
+    for (let key in map) {
+        if (map[key].size == 0) {
+            queue.push(key)
+            res.push(+key)
+            delete map[key]
+        }
+    }
+
+    while (queue.length) {
+        let independentCourse = queue.shift();
+        for (let key in map) {
+            if (map[key].has(+independentCourse)) {
+                map[key].delete(+independentCourse)
+                if (map[key].size == 0) {
+                    queue.push(key)
+                    res.push(+key)
+                    delete map[key]
+                }
+            }
+        }
+    }
+
+    if (Object.keys(map).length > 0) {
+        return []
+    }
+
+    return res
+
+}
